ee133cedbbdd468dec09ab9e5600b808439c36b2
Lu Yao <ylu@da2-ex-dk107-11.helsinki.fi>
2016-09-15 22:43:45 +0300

    scroll speed

--- dosnes/demo/nips/js/dosnes.js
+++ dosnes/demo/nips/js/dosnes.js
@@ -248,7 +248,7 @@ function onDocumentMouseOut( event ) {
 
 function onDocumentMouseWheel( event ) {      
     var d = ((typeof event.wheelDelta != "undefined") ? (-event.wheelDelta) : event.detail);
-    camera.position.z += 0.02*d;       
+    camera.position.z += 0.005*d;       
 
     if (camera.position.z < 1.1) {
         camera.position.z = 1.1;
