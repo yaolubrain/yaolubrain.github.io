8b8a7f52fb49fc26d1787ab41cecb5b5444a8265
yao <yaolubrain@gmail.com>
2017-09-23 21:11:39 +1000

    up

---
+++ kube/js/kube.js
@@ -0,0 +1,68 @@
+class Point {
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  project() {
+    this.x_proj = 50*this.x / this.z;
+    this.y_proj = 50*this.y / this.z;
+    return this;
+  }
+
+  display() {
+    fill(0);
+    ellipse(this.x_proj, this.y_proj, 100/this.z, 100/this.z);
+    return this;
+  }
+
+  rotate() {
+    let theta = radians(2);
+    let x_old = this.x;
+    let z_old = this.z;
+    this.x =  0 + cos(theta)*x_old - sin(theta)*(z_old - 20);
+    this.z = 20 + sin(theta)*x_old + cos(theta)*(z_old - 20);
+    return this;
+  }
+}
+
+let points = [];
+
+function drawLine(p1, p2) {
+  stroke(0);
+  line(p1.x_proj, p1.y_proj, p2.x_proj, p2.y_proj);
+}
+
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  points.push(new Point(-10, -10, 10));
+  points.push(new Point( 10, -10, 10));
+  points.push(new Point( 10,  10, 10));
+  points.push(new Point(-10,  10, 10));
+  points.push(new Point(-10, -10, 30));
+  points.push(new Point( 10, -10, 30));
+  points.push(new Point( 10,  10, 30));
+  points.push(new Point(-10,  10, 30));
+}
+
+
+function draw() {
+  background(255);
+  translate(window.innerWidth/2, window.innerHeight/2);
+
+  points.forEach(p => p.rotate().project().display());
+
+  drawLine(points[0], points[1]);
+  drawLine(points[1], points[2]);
+  drawLine(points[2], points[3]);
+  drawLine(points[3], points[0]);
+  drawLine(points[4], points[5]);
+  drawLine(points[5], points[6]);
+  drawLine(points[6], points[7]);
+  drawLine(points[7], points[4]);
+  drawLine(points[0], points[4]);
+  drawLine(points[1], points[5]);
+  drawLine(points[2], points[6]);
+  drawLine(points[3], points[7]);
+}
