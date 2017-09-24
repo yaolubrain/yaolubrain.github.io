22f6ec7f1ee9e663afc69223e926eb8bcaa30091
yaolubrain <yaolubrain@gmail.com>
2017-09-24 22:02:29 +1000

    up

--- kube/sketch.js
+++ kube/sketch.js
@@ -6,18 +6,19 @@ class Point {
   }
 
   project() {
-    this.x_proj = 50*this.x / this.z;
-    this.y_proj = 50*this.y / this.z;
+    this.x_proj = 150*this.x / this.z;
+    this.y_proj = 150*this.y / this.z;
     return this;
   }
 
   display() {
     fill(0);
-    ellipse(this.x_proj, this.y_proj, 100/this.z, 100/this.z);
+//    ellipse(this.x_proj, this.y_proj, 50/this.z, 50/this.z);
+    ellipse(this.x_proj, this.y_proj, 0.1*this.z, 0.1*this.z);
     return this;
   }
 
-  rotate() {
+  rotateR() {
     let theta = radians(2);
     let x_old = this.x;
     let z_old = this.z;
@@ -25,9 +26,20 @@ class Point {
     this.z = 20 + sin(theta)*x_old + cos(theta)*(z_old - 20);
     return this;
   }
+
+  rotateL() {
+    let theta = -radians(2);
+    let x_old = this.x;
+    let z_old = this.z;
+    this.x =  0 + cos(theta)*x_old - sin(theta)*(z_old - 20);
+    this.z = 20 + sin(theta)*x_old + cos(theta)*(z_old - 20);
+    return this;
+  }
+ 
 }
 
-let points = [];
+let points_1 = [];
+let points_2 = [];
 
 function drawLine(p1, p2) {
   stroke(0);
@@ -36,33 +48,58 @@ function drawLine(p1, p2) {
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  points.push(new Point(-10, -10, 10));
-  points.push(new Point( 10, -10, 10));
-  points.push(new Point( 10,  10, 10));
-  points.push(new Point(-10,  10, 10));
-  points.push(new Point(-10, -10, 30));
-  points.push(new Point( 10, -10, 30));
-  points.push(new Point( 10,  10, 30));
-  points.push(new Point(-10,  10, 30));
+  points_1.push(new Point(-10, -10, 10));
+  points_1.push(new Point( 10, -10, 10));
+  points_1.push(new Point( 10,  10, 10));
+  points_1.push(new Point(-10,  10, 10));
+  points_1.push(new Point(-10, -10, 30));
+  points_1.push(new Point( 10, -10, 30));
+  points_1.push(new Point( 10,  10, 30));
+  points_1.push(new Point(-10,  10, 30));
+
+  points_2.push(new Point(-5, -5, 15));
+  points_2.push(new Point( 5, -5, 15));
+  points_2.push(new Point( 5,  5, 15));
+  points_2.push(new Point(-5,  5, 15));
+  points_2.push(new Point(-5, -5, 25));
+  points_2.push(new Point( 5, -5, 25));
+  points_2.push(new Point( 5,  5, 25));
+  points_2.push(new Point(-5,  5, 25));
+
 }
 
 
 function draw() {
   background(255);
-  translate(width/2, height/2);
+  translate(window.innerWidth/2, window.innerHeight/2);
 
-  points.forEach(p => p.rotate().project().display());
+  points_1.forEach(p => p.rotateL().project().display());
+  points_2.forEach(p => p.rotateR().project().display());
 
-  drawLine(points[0], points[1]);
-  drawLine(points[1], points[2]);
-  drawLine(points[2], points[3]);
-  drawLine(points[3], points[0]);
-  drawLine(points[4], points[5]);
-  drawLine(points[5], points[6]);
-  drawLine(points[6], points[7]);
-  drawLine(points[7], points[4]);
-  drawLine(points[0], points[4]);
-  drawLine(points[1], points[5]);
-  drawLine(points[2], points[6]);
-  drawLine(points[3], points[7]);
+  drawLine(points_1[0], points_1[1]);
+  drawLine(points_1[1], points_1[2]);
+  drawLine(points_1[2], points_1[3]);
+  drawLine(points_1[3], points_1[0]);
+  drawLine(points_1[4], points_1[5]);
+  drawLine(points_1[5], points_1[6]);
+  drawLine(points_1[6], points_1[7]);
+  drawLine(points_1[7], points_1[4]);
+  drawLine(points_1[0], points_1[4]);
+  drawLine(points_1[1], points_1[5]);
+  drawLine(points_1[2], points_1[6]);
+  drawLine(points_1[3], points_1[7]);
+
+  drawLine(points_2[0], points_2[1]);
+  drawLine(points_2[1], points_2[2]);
+  drawLine(points_2[2], points_2[3]);
+  drawLine(points_2[3], points_2[0]);
+  drawLine(points_2[4], points_2[5]);
+  drawLine(points_2[5], points_2[6]);
+  drawLine(points_2[6], points_2[7]);
+  drawLine(points_2[7], points_2[4]);
+  drawLine(points_2[0], points_2[4]);
+  drawLine(points_2[1], points_2[5]);
+  drawLine(points_2[2], points_2[6]);
+  drawLine(points_2[3], points_2[7]);
+
 }
