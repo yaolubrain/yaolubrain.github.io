65f05503ba14fc151dcbd62a8eb481b7abde3efe
Yao Lu <yao.lu@anu.edu.au>
2018-11-24 18:09:08 +1100

    m

--- delirium/sketch.js
+++ delirium/sketch.js
@@ -7,7 +7,7 @@ let t_off = 0;
 function setup() {
   createCanvas(W, H);
   img_old = loadImage("starry_night.jpg");
-  img_new = createImage(W, H);
+  img_new = createImage(W-100, H-100);
 }
 
 
@@ -20,24 +20,19 @@ function draw() {
 
   let y_off1 = 0;
   let y_off2 = 10;
-	for (let x = 0; x < W; x += psize) {
+	for (let x = 0; x < W-100; x += psize) {
     let x_off1 = 0;
     let x_off2 = 0;
-		for (let y = 0; y < H; y += psize) {
+		for (let y = 0; y < H-100; y += psize) {
       let i_new = int(map(noise(x_off1,y_off1,t_off), 0, 1, -50, 50));
       let j_new = int(map(noise(x_off2,y_off2,t_off), 0, 1, -50, 50));
 
       for (let i = 0; i < psize; ++i) {
         for (let j = 0; j < psize; ++j) {
-          let loc1 = (x+i + (y+j) * W) * 4;
-          img_new.pixels[loc1 + 0] = 0;
-          img_new.pixels[loc1 + 1] = 0;
-          img_new.pixels[loc1 + 2] = 0;
-          img_new.pixels[loc1 + 3] = 255;
+          let loc1 = (x+i + (y+j) * (W-100)) * 4;
 
-
-          let x_new = x+i+i_new;
-          let y_new = y+j+j_new;
+          let x_new = x+50+i+i_new;
+          let y_new = y+50+j+j_new;
 
           if (x_new < 0 || x_new >= W || y_new < 0 || y_new >= H) {
             continue;
