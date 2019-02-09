c3d1d9e3a45f5f455e72ee7971a67123e19650cd
Yao Lu <yao.lu@anu.edu.au>
2019-02-09 11:47:36 +1100

    matrxi

--- matrix/sketch.js
+++ matrix/sketch.js
@@ -18,15 +18,17 @@ class Square {
   display(t) {
 
     push();
-//    translate(this.x2, this.y2);
-    //    rotate(-i);
+
+    translate(window.innerWidth/2, window.innerHeight/2);
+    translate((this.x1+this.x3)/2, (this.y1+this.y3)/2);
+    rotate(0.1*t);
+
     let ratio = (cos(t) + 1) / 2;
     if (ratio > 0.5) {
-      quad(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3, this.x4, this.y4);
-      
+      quad(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3, this.x4, this.y4);      
     } else {
       noFill();
-      strokeWeight(5);
+      strokeWeight(2);
       quad(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3, this.x4, this.y4);
     }
     
@@ -47,47 +49,43 @@ let squares = [];
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-//  translate(window.innerWidth/2, window.innerHeight/2);
+
   for (let i = -20; i < 20; i++) {
     for (let j = -20; j < 20; j++) {
+      let n1 = 5;
+      let n2 = 5;
 
-      let n1 = map(noise(i, j), 0, 1, -10, 10);
-      let n2 = map(noise(i+5, j+5), 0, 1, -10, 10);
-      n1 = 5;
-      n2 = 5;
-      let x1 = i*80 + n1;
-      let y1 = j*80 + n2;
-      let x2 = i*80 + 60;
-      let y2 = j*80;
-      let x3 = i*80 + 60 - n1;
-      let y3 = j*80 + 60 - n2;
-      let x4 = i*80;
-      let y4 = j*80 + 60;
-      squares.push(new Square(x1, y1, x2, y2, x3, y3, x4, y4));
+      let x1 = i*60 + n1;
+      let y1 = j*60 + n2;
+      let x2 = i*60 + 40;
+      let y2 = j*60;
+      let x3 = i*60 + 40 - n1;
+      let y3 = j*60 + 40 - n2;
+      let x4 = i*60;
+      let y4 = j*60 + 40;
+      
+      if (i % 2 == 0) {      
+	squares.push(new Square(x1, y1, x2, y2, x3, y3, x4, y4));
+      } else {
+	squares.push(new Square(x3, y3, x2, y2, x1, y1, x4, y4));
+      }
 
     }	
   }  
 }
 
-index = 1;
+let t = 1;
 
 function draw() {
   background(255);
-  translate(window.innerWidth/2, window.innerHeight/2);
-//  squares.forEach(sq => sq.display());
 
-
-  for (let i = 0; i < squares.length; ++i) {
-    squares[i].move(0.02*index);    
-    squares[i].display(0.02*index);
-
+  for (let i = -20; i < 20; i++) {
+    for (let j = -20; j < 20; j++) {
+      let index = (i+20) * 40 + j + 20;
+      squares[index].move(0.05*t + 0.1*(i-j) + 0.6*(j+i));
+      squares[index].display(0.05*t + 0.1*(i-j) + 0.6*(j+i) );
+    }
   }
 
-  index++;
-
-
-  
-//  translate(window.innerWidth/2, window.innerHeight/2);
-
-
+  t++;
 }
